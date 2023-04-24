2284e8adf961f6b3aedb8b70734dc6bf29bce254
Tamara Leonard <LeonardTamaraD@gmail.com>
2023-04-24 17:51:00 -0500

    chngd stff

--- scriptHelper.js
+++ scriptHelper.js
@@ -46,18 +46,18 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     }
    
    if(Number(fuelLevel) < 10000){
-    //list.style.visiblity = 'visible';
+    list.style.visiblity = 'visible';
     fuelStatus.innerHTML  = 'Fuel level too low for launch';
     launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
     launchStatus.style.color = 'rgb(199, 37, 78)';
    } else if(Number(cargoLevel) > 10000){
-    //list.style.visiblity = 'visible';
+    list.style.visiblity = 'visible';
     fuelStatus.innerHTML = 'Fuel level high enough for launch'
     cargoStatus.innerHTML  = 'Cargo mass too heavy for launch';
     launchStatus.innerHTML = 'Shuttle Not Ready for Launch';
     launchStatus.style.color = 'rgb(199, 37, 78)';
    } else if(Number(fuelLevel) >= 10000 && Number(cargoLevel) <= 10000) {
-    //list.style.visiblity = 'hidden';
+    list.style.visiblity = 'hidden';
     fuelStatus.innerHTML = 'Fuel level high enough for launch';
     cargoStatus.innerHTML = 'Cargo mass low enough for launch';
     launchStatus.innerHTML = 'Shuttle is Ready for Launch';
