b37b46306171f3da52a98902245be880881e1c2f
Tamara Leonard <LeonardTamaraD@gmail.com>
2023-04-24 21:55:47 -0500

    spelling

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.addEventListener("load", function() {
     })
 
     let list = document.getElementById('faultyItems');
-    list.style.visiblity = 'hidden';
+    list.style.visibility = 'visible';
     let form = document.querySelector('form');
     form.addEventListener("submit", function(event){
         event.preventDefault();
